46f1bba8f707378c9c87fca9a7f6fde0ad60ee4c
OliviaRS <writingbloc99@gmail.com>
2016-10-10 16:48:36 -0700

    Added static const int's to all the buttons to match the values

--- src/OI.h
+++ src/OI.h
@@ -41,6 +41,16 @@ private:
 	double getDriverLeftAxis() const;
 	double getDriverRightAxis() const;
 
+	static const int kButtonA_ID = 1;
+	static const int kButtonB_ID = 2;
+	static const int kButtonX_ID = 3;
+	static const int kButtonY_ID = 4;
+	static const int kButtonLB_ID = 5;
+	static const int kButtonRB_ID = 6;
+	static const int kButtonBack_ID = 7;
+	static const int kButtonStart_ID = 8;
+	static const int kButtonL3_ID = 9;
+	static const int kButtonR3_ID = 10;
 };
 
 #endif
