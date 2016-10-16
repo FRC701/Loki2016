d637db4f950665fd6561c8f6dab276f5abd4e563
OliviaRS <writingbloc99@gmail.com>
2016-10-16 01:08:15 -0700

    Add Commands for Driver Controls
    - brake toggle
    - cancel command

--- src/OI.h
+++ src/OI.h
@@ -5,38 +5,50 @@
 
 class OI {
 private:
-	std::shared_ptr<Joystick> driver;
+  std::shared_ptr<Joystick> driver;
 
-	std::shared_ptr<JoystickButton> dA;
-	std::shared_ptr<JoystickButton> dB;
-	std::shared_ptr<JoystickButton> dX;
-	std::shared_ptr<JoystickButton> dY;
-	std::shared_ptr<JoystickButton> dLB;
-	std::shared_ptr<JoystickButton> dRB;
-	std::shared_ptr<JoystickButton> dL3;
-	std::shared_ptr<JoystickButton> dR3;
-	std::shared_ptr<JoystickButton> dStart;
-	std::shared_ptr<JoystickButton> dBack;
+  std::shared_ptr<JoystickButton> dA;
+  std::shared_ptr<JoystickButton> dB;
+  std::shared_ptr<JoystickButton> dX;
+  std::shared_ptr<JoystickButton> dY;
+  std::shared_ptr<JoystickButton> dLB;
+  std::shared_ptr<JoystickButton> dRB;
+  std::shared_ptr<JoystickButton> dL3;
+  std::shared_ptr<JoystickButton> dR3;
+  std::shared_ptr<JoystickButton> dStart;
+  std::shared_ptr<JoystickButton> dBack;
 
+  std::shared_ptr<Joystick> coDriver;
 
-	public:
-	OI();
+  std::shared_ptr<JoystickButton> coA;
+  std::shared_ptr<JoystickButton> coB;
+  std::shared_ptr<JoystickButton> coX;
+  std::shared_ptr<JoystickButton> coY;
+  std::shared_ptr<JoystickButton> coLB;
+  std::shared_ptr<JoystickButton> coRB;
+  std::shared_ptr<JoystickButton> coL3;
+  std::shared_ptr<JoystickButton> coR3;
+  std::shared_ptr<JoystickButton> coStart;
+  std::shared_ptr<JoystickButton> coBack;
 
-	std::shared_ptr<Joystick> getDriver();
+public:
+  OI();
 
-	double getDriverLeftAxis() const;
-	double getDriverRightAxis() const;
+  std::shared_ptr<Joystick> getDriver();
 
-	static const int kButtonA_ID = 1;
-	static const int kButtonB_ID = 2;
-	static const int kButtonX_ID = 3;
-	static const int kButtonY_ID = 4;
-	static const int kButtonLB_ID = 5;
-	static const int kButtonRB_ID = 6;
-	static const int kButtonBack_ID = 7;
-	static const int kButtonStart_ID = 8;
-	static const int kButtonL3_ID = 9;
-	static const int kButtonR3_ID = 10;
+  double getDriverLeftAxis() const;
+  double getDriverRightAxis() const;
+
+  static const int kButtonA_ID = 1;
+  static const int kButtonB_ID = 2;
+  static const int kButtonX_ID = 3;
+  static const int kButtonY_ID = 4;
+  static const int kButtonLB_ID = 5;
+  static const int kButtonRB_ID = 6;
+  static const int kButtonBack_ID = 7;
+  static const int kButtonStart_ID = 8;
+  static const int kButtonL3_ID = 9;
+  static const int kButtonR3_ID = 10;
 };
 
 #endif
