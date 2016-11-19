edb30a810ddbdc5f52fc82bcde419c9ce1e97293
Duane Murphy <duanemurphy@mac.com>
2016-11-18 20:35:04 -0800

    Whitespace Cleanup
    Cleanup whitespace in a couple of files.

--- src/Commands/SetShifter.cpp
+++ src/Commands/SetShifter.cpp
@@ -1,11 +1,10 @@
 #include "SetShifter.h"
+#include "../Robot.h"
 
 SetShifter::SetShifter(Chassis::ShifterValue shifterValue)
 : mShifterValue(shifterValue)
 {
-	// Use Requires() here to declare subsystem dependencies
-	// eg. Requires(chassis);
-	Requires(Robot::chassis.get());
+  Requires(Robot::chassis.get());
 }
 
 // Called just before this Command runs the first time
@@ -17,13 +16,13 @@ void SetShifter::Initialize()
 // Called repeatedly when this Command is scheduled to run
 void SetShifter::Execute()
 {
-	Robot::chassis->SetShifter(mShifterValue);
+  Robot::chassis->SetShifter(mShifterValue);
 }
 
 // Make this return true when this Command no longer needs to run execute()
 bool SetShifter::IsFinished()
 {
-	return true;
+  return true;
 }
 
 // Called once after isFinished returns true
