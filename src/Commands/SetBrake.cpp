2f003e28eab5237cb9a817b5941c89c90589ff0b
Duane Murphy <duanemurphy@mac.com>
2016-12-12 21:34:29 -0800

    fixup! Add Commands for Driver Controls

--- src/Commands/SetBrake.cpp
+++ src/Commands/SetBrake.cpp
@@ -12,19 +12,18 @@ SetBrake::SetBrake(Chassis::TalonMode mode)
 // Called just before this Command runs the first time
 void SetBrake::Initialize()
 {
-
+  Robot::chassis->SetMode(mMode);
 }
 
 // Called repeatedly when this Command is scheduled to run
 void SetBrake::Execute()
 {
-  Robot::chassis->SetMode(mMode);
 }
 
 // Make this return true when this Command no longer needs to run execute()
 bool SetBrake::IsFinished()
 {
-  return false;
+  return true;
 }
 
 // Called once after isFinished returns true
