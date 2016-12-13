71bad6b90b10cc7a37ebc6512a4f342b8075acd0
OliviaRS <writingbloc99@gmail.com>
2016-12-12 18:26:40 -0800

    Commit left over code

--- src/Commands/RollerOn.h
+++ src/Commands/RollerOn.h
@@ -2,19 +2,21 @@
 #define RollerOn_H
 
 #include "Commands/Command.h"
+#include "../Subsystems/Shooter.h"
+#include "../Robot.h"
 
 class RollerOn: public Command
 {
 public:
-  RollerOn(double speed);
-  void Initialize();
-  void Execute();
-  bool IsFinished();
-  void End();
-  void Interrupted();
+	RollerOn(double speed);
+	void Initialize();
+	void Execute();
+	bool IsFinished();
+	void End();
+	void Interrupted();
 
 private:
-  double mSpeed;
+	double mSpeed;
 };
 
 #endif
